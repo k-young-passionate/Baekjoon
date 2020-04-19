5617377b84f9e71a5d634d3bb73419464e285df8
Keonyoung Shim <kyshim9004@gmail.com>
2020-04-19 13:20:57 +0900

    prime & palindrome [Gold]

--- python_version/p1747.py
+++ python_version/p1747.py
@@ -1,20 +1,41 @@
+from math import sqrt
+def ispalindrome(num):
+    n = str(num)
+
+    front = n[:len(n)//2]
+    if len(n)%2 == 0:
+        addition = 0
+    else:
+        addition = 1
+    back = n[len(n)//2+addition:]
+    back = back[::-1]
+
+    if front == back:
+        return True
+    else:
+        return False
+
+def isprime(num):
+    if num == 1:
+        return False
+    s = int(sqrt(num))
+    for i in range(2, s+1):
+        if num % i == 0:
+            return False
+    return True
+
 
 
 n = int(input())
 
-i = 2
+i = n
 prime = []
 
 while True:
-    if len(prime) == 0:
-        prime.append(i)
-    else:
-        flag = True
-        for p in prime:
-            if i % p == 0:
-                break
-        if flag:
+    flag = True
+    if ispalindrome(i):
+        if isprime(i):
+            print(i)
             break
-        break
-
-print(prime)
+    i += 1
+# print(prime)
