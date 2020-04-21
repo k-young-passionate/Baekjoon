4faaefc91337e2b7c8da05fa8afcb8084017547e
Keonyoung Shim <kyshim9004@gmail.com>
2020-04-21 17:23:08 +0900

    ramp [Gold] _ add comments

--- python_version/p14890.py
+++ python_version/p14890.py
@@ -8,66 +8,73 @@ def getinput():
     return n, l, roads
 
 
-def roadcheck(n, l, road):
+def roadcheck(n, l, road): # checks a road valid or not, N
     ramp = []
     flag = 0
     now = 0
     ramp.append(0)
-    result = False
-    for i in range(len(road)-1):
+
+    for i in range(len(road)-1):  # left to right traverse
         r = 0
-        if flag == 0:
-            if road[i] == road[i+1] + 1:
+        if flag == 0:  # normal cases
+            if road[i] == road[i+1] + 1:  # starts to add ramps
                 flag = 1
                 now = road[i+1]
-            elif road[i] > road[i+1] + 1:
-                return result
-        if 1 <= flag <= l:
-            if road[i+1] == now:
-                if flag == l:
+            elif road[i] > road[i+1] + 1:  # gap more than 2
+                return False
+
+        if 1 <= flag <= l:  # cases adding ramps
+            if road[i+1] == now:  # checks whether it is valid spaces
+                if flag == l:  # end of the ramp
                     flag = 0
-                else:
+                else:  # middle of the ramp
                     flag += 1
                 r = 1
-            else:
+            else:  # invalid space
                 return False
         ramp.append(r)
-    if flag > 0:
+
+    if flag > 0:  # handle uncompleted finish
         return False
+
     flag = 0
-    for i in range(len(road)-1, 0, -1):
+    for i in range(len(road)-1, 0, -1):  # right to left traverse
         r = 0
-        if flag == 0:
-            if road[i] == road[i - 1] + 1:
+        if flag == 0:  # normal cases
+            if road[i] == road[i - 1] + 1:  # starts to add ramps
                 flag = 1
                 now = road[i-1]
-            elif road[i] > road[i - 1] + 1:
-                return result
-        if 1 <= flag <= l:
-            if road[i - 1] == now:
+            elif road[i] > road[i - 1] + 1:  # gap more than 2
+                return False
+        if 1 <= flag <= l:  # cases adding ramps
+            if road[i - 1] == now:  # checks whether it is valid spaces
                 r = 1
-                if flag == l:
+                if flag == l:  # end of the ramp
                     flag = 0
-                else:
+                else:  # middle of the ramp
                     flag += 1
-            else:
+            else:  # invalid space
                 return False
         ramp[i-1] += r
-    if flag > 0:
+
+    if flag > 0:  # handle uncompleted finish
         return False
-    for i in ramp:
+
+    for i in ramp:  # checks whether there is any duplicated ramps on a same place
         if i > 1:
-            return result
+            return False
+
     return True
 
 
-def check(n, l, roads):
+def check(n, l, roads):  # checks road one by one and counts the number of valid roads, N
     result = 0
-    for i in range(n):
+    for i in range(n):  # horizontal check
         road = roads[i].copy()
         if roadcheck(n, l, road):
             result += 1
-    for i in range(n):
+
+    for i in range(n):  # vertical check
         road = []
         for j in range(n):
             road.append(roads[j][i])
