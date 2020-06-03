4e8157159ebf6fd99fa3a6de6c5cc6c76dfe78f1
Keonyoung Shim <kyshim9004@gmail.com>
2020-06-04 03:27:29 +0900

    comment added

--- swexpert/p5658.py
+++ swexpert/p5658.py
@@ -7,7 +7,7 @@ def getinput():
     return n, k, square
 
 
-def tonumber(square):
+def tonumber(square):  # 16진수를 10진수로
     nums = [str(x) for x in range(10)]
     nums.append('A')
     nums.append('B')
@@ -27,19 +27,19 @@ def tonumber(square):
     num //= 16
     return num
 
+
 if __name__ == "__main__":
     test_case = int(input())
     for t in range(test_case):
         nums = []
         n, k, square = getinput()
-        for i in range(n//4):
-            for j in range(4):
-                num = tonumber(square[j*(n//4):j*(n//4)+n//4])
-                if num not in nums:
+        for i in range(n//4):  # 총 시도해야하는 횟수
+            for j in range(4):  # 체크할 변의 수
+                num = tonumber(square[j*(n//4):j*(n//4)+n//4])  # 수 뽑아내기
+                if num not in nums:  # 중복 방지 추가
                     nums.append(num)
             tmp = square[0]
             square = square[1:]
-            square.append(tmp)
-        nums.sort(reverse=True)
-        # print(nums)
-        print("#"+str(t+1),nums[k-1])
+            square.append(tmp)  # 하나씩 움직이기
+        nums.sort(reverse=True)  # 큰 수부터 정렬
+        print("#"+str(t+1),nums[k-1])  # k번째 값 출력
