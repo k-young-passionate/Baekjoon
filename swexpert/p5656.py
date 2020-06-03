456d467871752b8afb981efb2f8b15171f6f1c60
Keonyoung Shim <kyshim9004@gmail.com>
2020-06-04 03:24:43 +0900

    comment added

--- swexpert/p5656.py
+++ swexpert/p5656.py
@@ -5,45 +5,37 @@ from itertools import product
 def getinput():
     n, w, h = map(int, input().split())
     bricks = [list(map(int, input().split())) for x in range(h)]
-    bombed = []
-    tmp = []
-    for _ in range(w):
-        tmp.append(False)
+    bombed = []  # 필요 없음
 
-    for _ in range(w):
-        bombed.append(tmp.copy())
-
-    originsize = 0
+    originsize = 0  # 원래 사이즈
     for i in range(h):
         for j in range(w):
             if bricks[i][j] > 0:
                 originsize += 1
 
-    droppos = product(range(w), repeat=n)
+    droppos = product(range(w), repeat=n)  # 모든 경우의 수, 중복순열
     return n, w, h, bricks, bombed, droppos, originsize
 
 
 def simulation(w, h, pos, bricks, bombed):
-    sizes = 0
+    sizes = 0  # 총 사라진 벽돌
     height = -1
-    for j in range(h):
+    for j in range(h):  # 해당하는 column 벽돌 찾기
         size = bricks[j][pos]
         if size > 0:
             height = j
             break
-    if height == -1:
-        return False
-    stack, size = bombing(w, h, (height, pos), size, bricks)
-    sizes += size
-    while len(stack) > 0:
+    if height == -1:  # 없으면 return
+        return 0
+    stack, size = bombing(w, h, (height, pos), size, bricks)  # 해당 벽독 파괴하고, splash damage 값 저장
+    sizes += size  # 터진 벽돌 수 저장
+    while len(stack) > 0:  # 연쇄작용으로 터질 벽돌들 계속 터뜨림
         s = stack.pop()
         tmpl, size = bombing(w, h, s[0], s[1], bricks)
         sizes += size
         for t in tmpl:
             stack.append(t)
-        # print(stack)
-        # debug(bricks)
-    dropdown(w, h, bricks)
+    dropdown(w, h, bricks)  # 벽돌 내리기
     return sizes
 
 
@@ -53,7 +45,7 @@ def debug(bricks):
         print(i)
 
 
-def dropdown(w, h, bricks):
+def dropdown(w, h, bricks):  # 중간에 0 이면 내리기
     for j in range(w):
         for i in range(h-1, 0, -1):
             flag = True
@@ -68,55 +60,31 @@ def dropdown(w, h, bricks):
                     break
 
 
-def bombing(w, h, position, size, bricks):
-    candidates = []
-    result = 0
-    for i in range(position[0] - size + 1, position[0] + size):
-        if 0 <= i < h:
-            if bricks[i][position[1]] > 1:
+def bombing(w, h, position, size, bricks):  # 폭탄 터뜨리기
+    candidates = []  # 더 터질 후보들
+    result = 0  # 터진 벽돌들
+    for i in range(position[0] - size + 1, position[0] + size):  # 세로로 터뜨리기
+        if 0 <= i < h:  # 범위 체크
+            if bricks[i][position[1]] > 1:  # 더 터질 벽돌 세기
                 candidates.append([(i, position[1]), bricks[i][position[1]]])
-            if bricks[i][position[1]] >= 1:
+            if bricks[i][position[1]] >= 1:  # 파괴된 벽돌 세기
                 result += 1
             bricks[i][position[1]] = 0
-    for i in range(position[1] - size + 1, position[1] + size):
+    for i in range(position[1] - size + 1, position[1] + size):  # 가로로 터뜨리기
         if 0 <= i < w:
-            # print(position[0], i, bricks[position[0]][i])
             if bricks[position[0]][i] > 1:
                 candidates.append([(position[0], i), bricks[position[0]][i]])
             if bricks[position[0]][i] >= 1:
                 result += 1
             bricks[position[0]][i] = 0
 
-    # for i in range(lower, -1, -1):
-    #     if i - (lower - upper + 1) < 0:
-    #         bricks[i][position[1]] = 0
-    #     else:
-    #         bricks[i][position[1]] = bricks[i - (lower - upper + 1)][position[1]]
-    #
-    # for j in range(left , right+1):
-    #     for i in range(position[0], -1, -1):
-    #         if i == 0:
-    #             bricks[i][j] = 0
-    #         else:
-    #             bricks[i][j] = bricks[i-1][j]
     return candidates, result
 
-
-def count(bricks):
-    cnt = 0
-    for bb in bricks:
-        for b in bb:
-            if b > 0:
-                cnt += 1
-
-    return cnt
 
 
 test_case = int(input())
 for T in range(test_case):
     n, w, h, bricks, bombed, droppos, originsize = getinput()
-    # print('new era')
-    # debug(bricks)
     maxsize = 0
     for dp in droppos:
         size = 0
@@ -124,12 +92,8 @@ for T in range(test_case):
 
         for i in range(n):
             size += simulation(w, h, dp[i], new_bricks, bombed)
-            # debug(new_bricks)
-        # print(dp, size, maxsize)
-        # size = count(new_bricks)
         if size > maxsize:
             maxsize = size
         if maxsize == originsize:
             break
-    # print(originsize)
     print("#"+str(T+1), originsize - maxsize)
